f3b674a1388024a7a7fbdc82e8c17de5e8b0d3c1
Yang Zhou <deedecho@gmail.com>
2022-12-06 11:16:56 +0800

    update

--- init.lua
+++ init.lua
@@ -1,42 +1,41 @@
-require('basics')
-require('colors')
-require('coc-config')
-require('easy-motion-config')
-require('lualine').setup()
+require("basics")
+require("colors")
+require("coc-config")
+require("easy-motion-config")
+require("lualine").setup()
 
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = "maintained",
-  context_commentstring = {
-    enable = true
-  },
-  highlight = {
-    enable = true
-  },
-  indent = {
-    enable = true
-  }
-}
+require("nvim-treesitter.configs").setup({
+	context_commentstring = {
+		enable = true,
+	},
+	highlight = {
+		enable = true,
+	},
+	indent = {
+		enable = true,
+	},
+})
 
-return require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
-  use {'neoclide/coc.nvim', branch = 'release'}
-  use 'morhetz/gruvbox'
-  use 'nvim-treesitter/nvim-treesitter'
-  use 'tpope/vim-commentary'
-  use 'JoosepAlviste/nvim-ts-context-commentstring'
-  use 'ThePrimeagen/git-worktree.nvim'
-  use 'tpope/vim-fugitive'
-  -- use 'liuchengxu/eleline.vim'
-  use 'vim-airline/vim-airline'
-  use 'easymotion/vim-easymotion'
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = {'kyazdani42/nvim-web-devicons', opt = true}
-  }
-  use {
-    "blackCauldron7/surround.nvim",
-    config = function()
-      require"surround".setup {mappings_style = "surround"}
-    end
-  }
+return require("packer").startup(function()
+	use("wbthomason/packer.nvim")
+	use({ "neoclide/coc.nvim", branch = "release" })
+	use("morhetz/gruvbox")
+	use("nvim-treesitter/nvim-treesitter")
+	use("tpope/vim-commentary")
+	use("JoosepAlviste/nvim-ts-context-commentstring")
+	use("ThePrimeagen/git-worktree.nvim")
+	use("tpope/vim-fugitive")
+	-- use 'liuchengxu/eleline.vim'
+	use("vim-airline/vim-airline")
+	use("easymotion/vim-easymotion")
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
+	use({
+		"blackCauldron7/surround.nvim",
+		config = function()
+			require("surround").setup({ mappings_style = "surround" })
+		end,
+	})
 end)
--- lua/coc-config.lua
+++ lua/coc-config.lua
@@ -11,6 +11,7 @@ vim.api.nvim_set_keymap("i", "<CR>", "pumvisible() ? coc#_select_confirm() : '<C
 vim.api.nvim_set_keymap("n", "<C-e>", ":CocCommand explorer<CR>", { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-P>', ":CocList files<CR>", { noremap = true })
 vim.api.nvim_set_keymap('n', '<Space>e', ":CocList buffers<CR>", { noremap = true })
+vim.api.nvim_set_keymap('n', '<Space>f', ":CocList grep<CR>", { noremap = true })
 
 vim.o.hidden = true
 vim.o.backup = false
@@ -30,7 +31,7 @@ vim.g.coc_global_extensions = {
 	'coc-jest',
 	'coc-json',
 	'coc-lists',
-  'coc-lua',
+  'coc-sumneko-lua',
 	'coc-prettier',
 	'coc-pyright',
   'coc-rust-analyzer',
